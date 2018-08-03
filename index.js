26f68a73e89e9cf6ee871a69734ae4731fb4f4fb
zackcarlson <zackt.carlson@gmail.com>
2018-08-03 12:01:29 -0700

    Create function to test out babel spread operator

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello world!');
+const logSomething = options => ({
+  ...options,
+  anotherOption: 'Hello!'
+});
+const options = logSomething({ one: '1', two: '2' });
+console.log(options);
