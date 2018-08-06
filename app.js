cb6611e4a7b13ffd74388a7588b9a020648c5fef
zackcarlson <zackt.carlson@gmail.com>
2018-08-06 15:01:44 -0700

    Import path to join dirname with views filepath, use the Pug engine

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+import path from 'path';
 import express from 'express';
-import routes from './routes';
+import routes from './routes/index';
 
 const app = express();
+
+// view engine
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'pug');
+
+// use routes
 app.use('/', routes);
 
 export default app;
